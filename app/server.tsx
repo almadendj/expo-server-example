f8e32ead6345c2d50baff7af0ba2bd56c11e9d79
almadendj <almadendj@gmail.com>
2024-07-30 02:01:20 +0800

    initial commit

---
+++ app/server.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HtmlServer() {
+  return (
+    <ThemedView>
+      <ThemedText>Hello World!</ThemedText>
+    </ThemedView>
+  );
+}
